ae09c7d9e529b86810a3bca9dd0e13efd98f6b37
loamtor <118779491+loamtor@users.noreply.github.com>
2024-04-06 14:00:57 -0400

    Create SphereSurface.lua

---
+++ Tests/PoissonDisc/SphereSurface.lua
@@ -0,0 +1,38 @@
+--!strict
+
+-- Services:
+
+local ServerStorage         =   game:GetService("ServerStorage")
+local ReplicatedStorage     =   game:GetService("ReplicatedStorage")
+
+-- Libraries:
+
+local Debug             =   require(ReplicatedStorage["Debug"])
+local Algaerhythms      =   ServerStorage["Algaerhythms"]
+
+local PointDistribution =   require(Algaerhythms["PointDistribution"])
+local PoissonDisc       =   PointDistribution["PoissonDisc"]
+
+local DefaultSettings   =   PoissonDisc["DefaultSettings"]
+
+-- Test:
+
+local DebugSpace = Instance.new("Model")
+DebugSpace.Name = "SphereSurfacePoints"
+Debug.SetDebugSpace(DebugSpace)
+DebugSpace.Parent = workspace
+
+local SphereSurfacePoints   =   PoissonDisc.GeneratePointsOnSphereSurface(
+    100,
+    10000,
+    5,
+    DefaultSettings.MaxSampleAttempts,
+    DefaultSettings.RandomSource,
+    DefaultSettings.Epsilon,
+    Debug.Visualizers.ValidVisualizer,
+    Debug.Visualizers.Null,
+    Debug.Visualizers.Null,
+    Debug.Visualizers.Null,
+    Debug.Visualizers.Null
+)
+
